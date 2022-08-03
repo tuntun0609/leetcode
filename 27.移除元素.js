1ce28822ff5a13daf2b4fc74f17afd72cf024faf
tuntun <56946732+Nozomi669664@users.noreply.github.com>
2022-08-03 21:28:39 +0800

    fix: finish question 27

--- 27.移除元素.js
+++ 27.移除元素.js
@@ -11,7 +11,17 @@
  * @return {number}
  */
 var removeElement = function(nums, val) {
-
+	let left = 0;
+	let right = nums.length;
+	while(left < right) {
+		if (nums[left] === val) {
+			nums[left] = nums[right - 1];
+			right--;
+		} else {
+			left++;
+		}
+	}
+	return left;
 };
 // @lc code=end
 
