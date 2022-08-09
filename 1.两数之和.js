96fb96c30149c95d28ce8244a24d4f7c23b2d379
tuntun <56946732+tuntun0609@users.noreply.github.com>
2022-08-09 16:34:17 +0800

    feat: add question 1

--- 1.两数之和.js
+++ 1.两数之和.js
@@ -11,7 +11,14 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-
+	let map = new Map();
+	for(let i = 0; i < nums.length; i++){
+		if(map.has(target - nums[i])){
+			return [map.get(target - nums[i]), i];
+		}else{
+			map.set(nums[i], i);
+		}
+	}
 };
 // @lc code=end
 
