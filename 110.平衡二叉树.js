27a95a71bc54e41453811d02bf7d944a833e240a
tuntun <56946732+tuntun0609@users.noreply.github.com>
2022-08-09 18:46:11 +0800

    feat: add question 110 112

---
+++ 110.平衡二叉树.js
@@ -0,0 +1,37 @@
+/*
+ * @lc app=leetcode.cn id=110 lang=javascript
+ *
+ * [110] 平衡二叉树
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+var isBalanced = function(root) {
+	if (root == null) {
+		return true;
+	} else {
+		return Math.abs(height(root.left) - height(root.right)) <= 1
+			&& isBalanced(root.left)
+			&& isBalanced(root.right);
+	}
+};
+
+function height(node) {
+	if (node == null) {
+		return 0;
+	}
+	return Math.max(height(node.left), height(node.right)) + 1;
+}
+// @lc code=end
+
