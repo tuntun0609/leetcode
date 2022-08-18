ee7ceb7b34d8070b5a5334aefa4cdfa16b7a13ec
tuntun <56946732+tuntun0609@users.noreply.github.com>
2022-08-18 16:07:46 +0800

    feat: add question 169 171

---
+++ 169.多数元素.js
@@ -0,0 +1,17 @@
+/*
+ * @lc app=leetcode.cn id=169 lang=javascript
+ *
+ * [169] 多数元素
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var majorityElement = function(nums) {
+	nums.sort((a, b) => a - b);
+	return nums[Math.floor(nums.length / 2)];
+};
+// @lc code=end
+
---
+++ 171.excel-表列序号.js
@@ -0,0 +1,23 @@
+/*
+ * @lc app=leetcode.cn id=171 lang=javascript
+ *
+ * [171] Excel 表列序号
+ */
+
+// @lc code=start
+/**
+ * @param {string} columnTitle
+ * @return {number}
+ */
+var titleToNumber = function(columnTitle) {
+	let number = 0;
+	let multiple = 1;
+	for (let i = columnTitle.length - 1; i >= 0; i--) {
+			const k = columnTitle[i].charCodeAt() - 'A'.charCodeAt() + 1;
+			number += k * multiple;
+			multiple *= 26;
+	}
+	return number;
+};
+// @lc code=end
+
