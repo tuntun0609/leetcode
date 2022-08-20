ed623ad5ea6c073ae13bba3a67f51c50bf845527
tuntun <56946732+tuntun0609@users.noreply.github.com>
2022-08-20 23:38:09 +0800

    feat: add question 225 226

---
+++ 225.用队列实现栈.js
@@ -0,0 +1,59 @@
+/*
+ * @lc app=leetcode.cn id=225 lang=javascript
+ *
+ * [225] 用队列实现栈
+ */
+
+// @lc code=start
+
+var MyStack = function() {
+	this.queue = [];
+	this._queue = [];
+};
+
+/** 
+ * @param {number} x
+ * @return {void}
+ */
+MyStack.prototype.push = function(x) {
+	this.queue.push(x);
+};
+
+/**
+ * @return {number}
+ */
+MyStack.prototype.pop = function() {
+	while(this.queue.length > 1){
+		this._queue.push(this.queue.shift());
+	}
+	let ans = this.queue.shift();
+	while(this._queue.length){
+		this.queue.push(this._queue.shift());
+	}
+	return ans;
+};
+
+/**
+ * @return {number}
+ */
+MyStack.prototype.top = function() {
+	return this.queue.slice(-1)[0];
+};
+
+/**
+ * @return {boolean}
+ */
+MyStack.prototype.empty = function() {
+	return !this.queue.length;
+};
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * var obj = new MyStack()
+ * obj.push(x)
+ * var param_2 = obj.pop()
+ * var param_3 = obj.top()
+ * var param_4 = obj.empty()
+ */
+// @lc code=end
+
