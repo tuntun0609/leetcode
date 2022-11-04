9b2c5cf1265ce9ff8c46c6bf83a7a45cbe87965c
tuntun <56946732+tuntun0609@users.noreply.github.com>
2022-11-04 19:10:23 +0800

    feat: add question 606

---
+++ 606.根据二叉树创建字符串.js
@@ -0,0 +1,33 @@
+/*
+ * @lc app=leetcode.cn id=606 lang=javascript
+ *
+ * [606] 根据二叉树创建字符串
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {string}
+ */
+var tree2str = function(root) {
+	if (root === null) {
+		return '';
+	}
+	if (root.left === null && root.right === null) {
+		return `${root.val}`;
+	}
+	if (root.right === null) {
+		return root.val + '(' + tree2str(root.left) + ')';
+	}
+	return root.val + '(' + tree2str(root.left) + ')(' + tree2str(root.right) + ')';
+};
+// @lc code=end
+
