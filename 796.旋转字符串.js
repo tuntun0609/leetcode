a7d13f88f8d7fabc85c342d3799467d430daa877
tuntun <760523867@qq.com>
2023-01-24 10:37:17 +0800

    add question 796

---
+++ 796.旋转字符串.js
@@ -0,0 +1,16 @@
+/*
+ * @lc app=leetcode.cn id=796 lang=javascript
+ *
+ * [796] 旋转字符串
+ */
+
+// @lc code=start
+/**
+ * @param {string} s
+ * @param {string} goal
+ * @return {boolean}
+ */
+var rotateString = function (s, goal) {
+	return s.length === goal.length && (s + s).indexOf(goal) !== -1;
+};
+// @lc code=end
