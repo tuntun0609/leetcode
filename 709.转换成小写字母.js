bbbb481c678c0b5fc664ce18e133ba070cc15c2c
tuntun <760523867@qq.com>
2023-01-25 10:38:21 +0800

    add question 709

---
+++ 709.转换成小写字母.js
@@ -0,0 +1,16 @@
+/*
+ * @lc app=leetcode.cn id=709 lang=javascript
+ *
+ * [709] 转换成小写字母
+ */
+
+// @lc code=start
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var toLowerCase = function(s) {
+	return s.toLowerCase();
+};
+// @lc code=end
+
