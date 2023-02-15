93fc9d0d2d68e30498bf0c41ce92e470d80134c8
tuntun <760523867@qq.com>
2023-02-15 21:24:03 +0800

    add question 1025

---
+++ 1025.除数博弈.js
@@ -0,0 +1,16 @@
+/*
+ * @lc app=leetcode.cn id=1025 lang=javascript
+ *
+ * [1025] 除数博弈
+ */
+
+// @lc code=start
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var divisorGame = function(n) {
+	return n % 2 == 0;
+};
+// @lc code=end
+
